c5d20a82101861df4877edb3e3c1f0dc67fe8e88
AegisVP <vlad@pysarenko.com>
2022-06-01 02:43:21 +0300

    updated JS to accept forms

--- src/index.js
+++ src/index.js
@@ -4,20 +4,27 @@ const closeMenuBtnRefList = document.querySelectorAll('[data-menu-close]');
 const mobileMenuRef = document.querySelector('[data-menu]');
 
 const openModalBtnList = document.querySelectorAll('[data-modal-open]');
+const openModalFrmList = document.getElementsByTagName('form');
 const closeModalBtnList = document.querySelectorAll('[data-modal-close]');
 const modalRef = document.querySelector('[data-modal]');
 
 if (openMenuBtnRefList.length) for (openMenuBtnRef of openMenuBtnRefList) {openMenuBtnRef.addEventListener('click', openMenu);};
 if (closeMenuBtnRefList.length) for (closeMenuBtnRef of closeMenuBtnRefList) {closeMenuBtnRef.addEventListener('click', closeMenu);};
-if (openModalBtnList.length) for (openModalBtn of openModalBtnList) {openModalBtn.addEventListener('click', openModal);};
+if (openModalFrmList.length) for (openModalFrm of openModalFrmList) {openModalFrm.addEventListener('submit', openModalForm);}
+if (openModalBtnList.length)for (openModalBtn of openModalBtnList) {openModalBtn.addEventListener('click', openModal);};
 if (closeModalBtnList.length) for (closeModalBtn of closeModalBtnList) {closeModalBtn.addEventListener('click', closeModal);};
 
+function openModalForm(form) {
+  form?.preventDefault();
+  openModal();
+  this.reset();
+}
 function openModal() {
-  document.body.classList.add('menu-open');
+  document.body.classList.add('modal-open');
   modalRef.classList.remove('is-hidden');
 }
 function closeModal() {
-  document.body.classList.remove('menu-open');
+  document.body.classList.remove('modal-open');
   modalRef.classList.add('is-hidden');
 }
 
