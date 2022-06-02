da72b2f58eafd228003fa2a691bc7e4ba3d8f784
AegisVP <vlad@pysarenko.com>
2022-06-02 21:58:43 +0000

    Deploying to gh-pages from @ AegisVP/HTML-CSS_54_Group@23a180fa0715f8e11d435fbbf542290623fa4d70 🚀

--- index.c7c14265.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".swiper",{loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",type:"fraction"},effect:"fade",fadeEffect:{crossFade:!0}});
-//# sourceMappingURL=index.c7c14265.js.map
