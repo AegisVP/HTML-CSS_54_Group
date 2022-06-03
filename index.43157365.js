9167dbe3057804ee2c35a81ba554a9abe20810e5
AegisVP <vlad@pysarenko.com>
2022-06-03 13:36:29 +0000

    Deploying to gh-pages from @ AegisVP/HTML-CSS_54_Group@de8b138fb37a9b8858cf2ba1d46d09d1c2fbcc63 🚀

---
+++ index.43157365.js
@@ -0,0 +1,2 @@
+function readMore(){var e=document.getElementById("more"),n=document.getElementById("btn");"block"===e.style.display?(n.innerHTML="Read more",e.style.display="none"):(n.innerHTML="Less",e.style.display="block")}
+//# sourceMappingURL=index.43157365.js.map
--- index.466c92c6.js
+++
@@ -1,2 +0,0 @@
-
-//# sourceMappingURL=index.466c92c6.js.map
